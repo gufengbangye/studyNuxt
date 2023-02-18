3d825545cec08e88e9eaa880e98bcffae7a18356
gufengbangye <1135523598@qq.com>
2023-02-18 15:55:01 +0800

    layouts

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="common-layout">
+      <el-container>
+        <el-header>头部</el-header>
+        <el-container>
+          <el-aside width="200px">侧边栏</el-aside>
+          <el-main><slot /></el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </template>
+  
